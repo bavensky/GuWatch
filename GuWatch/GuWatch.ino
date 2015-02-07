c5cf7bc8c28c9aec1b409c96df1b5873e76ecc5c
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2015-02-08 00:21:36 +0700

    clock microview

--- GuWatch/GuWatch.ino
+++ GuWatch/GuWatch.ino
@@ -5,10 +5,11 @@
 RTC_DS1307 rtc;;
 
 
-void setup() {
+void setup() 
+{
   Wire.begin();
   rtc.begin();
-  rtc.adjust(DateTime(__DATE__, __TIME__));
+  //rtc.adjust(DateTime(__DATE__, __TIME__));
   uView.begin();				
 }
 
@@ -17,22 +18,27 @@ void loop () {
   
   DateTime now = rtc.now();
   
-  uView.setFontType(1); 
+  uView.setFontType(0); 
   uView.setCursor(0,0);
-  uView.print(" GU Watch");
+  uView.print(" GU Watch ");
  
   uView.setFontType(0);
   uView.setCursor(6,16);
+  if(now.hour() < 10) uView.print("0");
   uView.print(now.hour(), DEC);
   uView.print(":");
+  if(now.minute() < 10) uView.print("0");
   uView.print(now.minute(), DEC);
   uView.print(":");
+  if(now.second() < 10) uView.print("0");
   uView.print(now.second(), DEC);
   uView.print("  ");
   
   uView.setCursor(0,32);
+  if(now.day() < 10) uView.print("0");
   uView.print(now.day(), DEC);
   uView.print("/");
+  if(now.month() < 10) uView.print("0");
   uView.print(now.month(), DEC);
   uView.print("/");
   uView.print(now.year(), DEC);
